3383d8f80a26c2dd7b305d143f7a552edace63f4
Yrol <yrollgayanth@yahoo.com>
2020-07-13 23:11:48 +1000

    Article validation rules

--- src/app/Http/Controllers/Articles/ArticleController.php
+++ src/app/Http/Controllers/Articles/ArticleController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Articles;
 
 use App\Http\Controllers\Controller;
+use App\Rules\CategoryExists;
+use App\Rules\UniqueCategoryName;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
@@ -10,5 +12,13 @@ class ArticleController extends Controller
     public function update(Request $request)
     {
 
+        $categoryId = $request->input('category_id');
+        $this->validate($request, [
+            'title' => ['required', new UniqueCategoryName($categoryId)],
+            'category_id' => ['required', new CategoryExists($categoryId)],
+            'body' => ['required'],
+            'is_live' => ['required'],
+            'close_to_comments' => ['required']
+        ]);
     }
 }
