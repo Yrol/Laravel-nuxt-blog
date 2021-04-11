38ceb73c80022df0606fc8553fe392e906aefaf5
Yrol <yrollgayanth@yahoo.com>
2021-04-11 19:49:49 +1000

    Including the category ID

--- src/app/Http/Resources/CategoryResource.php
+++ src/app/Http/Resources/CategoryResource.php
@@ -16,6 +16,7 @@ class CategoryResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'title' => $this->title,
             'slug' => $this->slug,
             'articles_count' => $this->articles_count,
